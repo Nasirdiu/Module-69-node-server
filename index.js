b74efb8d62cdf05e6ec5e355d825d28e49fb54a1
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-25 22:28:18 +0600

    added

--- index.js
+++ index.js
@@ -105,6 +105,9 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("server is running");
 });
+app.get("/hero", (req, res) => {
+  res.send("Hero is running");
+});
 
 app.listen(port, () => {
   console.log("CRUD is Running", port);
